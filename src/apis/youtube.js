ff92671226e116246739970c2b9cf6f22563ff5e
jamievullo <jvullo@protonmail.com>
2020-02-01 10:05:58 -0500

    Added axios and youtube api params.

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const KEY = 'AIzaSyCrSuHI7SukfTu0Vo_SU1PVUrHDcGyHEPk';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})
