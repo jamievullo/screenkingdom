aaa1b54790e08bcbeb95f8f9908a4077fcdcc427
jamievullo <jvullo@protonmail.com>
2020-02-01 11:43:28 -0500

    Added VideoDetail file and function along with conditional for state of selected video(null)

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 class App extends React.Component {
 
@@ -22,8 +23,9 @@ class App extends React.Component {
     }
 
     onVideoSelect = (video) => {
-        console.log(video)
-        
+        // console.log(video)
+        this.setState({selectedVideo: video})
+
     }
 
 
@@ -31,7 +33,11 @@ class App extends React.Component {
         return (
         <div className="ui container">
             <SearchBar onTermSubmit={this.onTermSubmit} />
-            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList 
+                onVideoSelect={this.onVideoSelect} 
+                videos={this.state.videos}
+            />
         </div>
         )
     }
