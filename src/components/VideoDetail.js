715ce26bf89c7347dfff09cc98fe38d63069eb39
jamievullo <jvullo@protonmail.com>
2020-02-01 12:00:57 -0500

    Added title to iframe, added key: id to each video rendered.

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -11,7 +11,7 @@ if(!video) {
     return (
     <div>
         <div className="ui embed">
-            <iframe src={videoSrc} />
+            <iframe title="videoplayer" src={videoSrc} />
         </div>
         <div className="ui segment">
             <h4 className="ui header">{video.snippet.title}</h4>
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,11 @@ const VideoItem = ({video, onVideoSelect}) => {
     return (
         // onClick done inline to pass in selected video
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-        <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+        <img 
+            className="ui image" 
+            src={video.snippet.thumbnails.medium.url} 
+            alt={video.snippet.title} 
+        />
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
             </div>
